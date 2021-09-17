77b23a490c36c5e30f214412b9c9ad6805948866
ClimbWorm <31581047+ClimbWorm@users.noreply.github.com>
2021-09-17 11:19:22 -0500

    Delete BackTesting_2021.4.13 directory
    delete code for computing zigzag

--- BackTesting_2021.4.13/_test/test_source_data.py
+++
@@ -1,9 +0,0 @@
-
-from dc.source_data import *
-
-
-def test_get_source_data():
-    df = get_source_data_from_config('sc', 'YMH21')
-    print(21, df)
-    for i in range(10):
-        print(i, type(df.iloc[1, i]))
